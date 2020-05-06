95b827baf8159f3ddc531564901a973a1473dd5c
George <g.popov44@icloud.com>
2020-05-06 17:44:28 +0300

    +linter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,13 @@
-import gendiff from '../src/lib/getDiff.js';
 import fs from 'fs';
+import gendiff from '../src/lib/getDiff.js';
 
-describe ('test gendiff', () => {
-    const firstPath = '__tests__/JSON/before.json'; 
-    const secondPath = '__tests__/JSON/after.json';
-    const resultPath = '__tests__/JSON/result.txt';
+describe('test gendiff', () => {
+  const firstPath = '__tests__/JSON/before.json';
+  const secondPath = '__tests__/JSON/after.json';
+  const resultPath = '__tests__/JSON/result.txt';
 
-    const expectResult = fs.readFileSync(resultPath, 'utf-8');
-    console.log(expectResult);
-    
-    it('gendiff test JSON', () => {
-        expect(gendiff(firstPath, secondPath)).toEqual(expectResult);
-    });
+  const expectResult = fs.readFileSync(resultPath, 'utf-8');
+  it('gendiff test JSON', () => {
+    expect(gendiff(firstPath, secondPath)).toEqual(expectResult);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import program from 'commander';
 import getDiff from './lib/getDiff.js';
 
