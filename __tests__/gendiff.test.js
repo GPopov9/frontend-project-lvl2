c3502837687849d1e52999b49c47d78ff7a8a787
George <g.popov44@icloud.com>
2020-05-26 20:36:39 +0300

    +tests refactor

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,35 +1,19 @@
 import fs from 'fs';
 import gendiff from '../src/index.js';
 
-describe('Test gendiff', () => {
-  const expectedDefault = fs.readFileSync('__tests__/__fixtures__/result-default.txt', 'utf-8');
-  const expectedPlain = fs.readFileSync('__tests__/__fixtures__/result-plain.txt', 'utf-8');
-  const expectedJSON = fs.readFileSync('__tests__/__fixtures__/result-json.txt', 'utf-8');
+const extensions = ['.json', '.yml', '.ini'];
 
-  const firstPath = '__tests__/__fixtures__/before';
-  const secondPath = '__tests__/__fixtures__/after';
-
-  test.each([
-    [`${firstPath}.json`, `${secondPath}.json`, expectedDefault],
-    [`${firstPath}.ini`, `${secondPath}.ini`, expectedDefault],
-    [`${firstPath}.yml`, `${secondPath}.yml`, expectedDefault],
-  ])('Default Format Test for json, ini, yaml', (a, b, expected) => {
-    expect(gendiff(a, b)).toEqual(expected);
+const test = (ext, format) => {
+  it(`Test ${ext} with ${format}`, () => {
+    const before = `__tests__/__fixtures__/before${ext}`;
+    const after = `__tests__/__fixtures__/after${ext}`;
+    const expected = fs.readFileSync(`__tests__/__fixtures__/result-${format}.txt`, 'utf-8');
+    expect(gendiff(before, after, format)).toEqual(expected);
   });
+};
 
-  test.each([
-    [`${firstPath}.json`, `${secondPath}.json`, expectedPlain],
-    [`${firstPath}.ini`, `${secondPath}.ini`, expectedPlain],
-    [`${firstPath}.yml`, `${secondPath}.yml`, expectedPlain],
-  ])('Plain Format Test for json, ini, yaml', (a, b, expected) => {
-    expect(gendiff(a, b, 'plain')).toEqual(expected);
-  });
-
-  test.each([
-    [`${firstPath}.json`, `${secondPath}.json`, expectedJSON],
-    [`${firstPath}.ini`, `${secondPath}.ini`, expectedJSON],
-    [`${firstPath}.yml`, `${secondPath}.yml`, expectedJSON],
-  ])('JSON Format Test for json, ini, yaml', (a, b, expected) => {
-    expect(gendiff(a, b, 'json')).toEqual(expected);
-  });
+describe('gendiff', () => {
+  extensions.forEach((extension) => test(extension, 'default'));
+  extensions.forEach((extension) => test(extension, 'plain'));
+  extensions.forEach((extension) => test(extension, 'json'));
 });
