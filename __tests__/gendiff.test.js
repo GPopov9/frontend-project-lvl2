8b56413838fc09230a9bf1f6414ed50bf6c70d81
George <g.popov44@icloud.com>
2020-06-02 19:56:22 +0300

    +refactor

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,23 +1,15 @@
 import fs from 'fs';
 import gendiff from '../src/index.js';
 
+const types = ['json', 'yml', 'ini'];
+const formats = ['default', 'plain', 'json'];
+
 const expected = (format) => fs.readFileSync(`__tests__/__fixtures__/result-${format}.txt`, 'utf-8');
 const after = '__tests__/__fixtures__/after';
 const before = '__tests__/__fixtures__/before';
 
 describe('gendiff', () => {
-  test.each`
-  ext | format 
-  ${'.json'} | ${'default'}
-  ${'.ini'} | ${'default'} 
-  ${'.yml'} | ${'default'} 
-  ${'.json'} | ${'plain'} 
-  ${'.ini'} | ${'plain'} 
-  ${'.yml'} | ${'plain'} 
-  ${'.json'} | ${'json'} 
-  ${'.ini'} | ${'json'} 
-  ${'.yml'} | ${'json'}   
-  `('Test $ext file with $format format', ({ ext, format }) => {
-  expect(gendiff(`${before}${ext}`, `${after}${ext}`, format)).toBe(expected(format));
-});
+  formats.forEach((format) => test.each(types)(`Test %s file with ${format} format`, (type) => {
+    expect(gendiff(`${before}.${type}`, `${after}.${type}`, format)).toBe(expected(format));
+  }));
 });
